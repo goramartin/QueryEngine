a452751eb9589aa4534c934d7b6568aab1f87a80
Martin Gora <im.goramartin@gmail.com>
2019-09-27 14:51:02 +0200

    Main is now in loop for continual input of queries

--- QueryEngine/QueryEngine.cs
+++ QueryEngine/QueryEngine.cs
@@ -10,7 +10,6 @@ namespace QueryEngine
 {
     class QueryEngine
     {
-        
         private static Query CreateQuery(TextReader reader,
             Dictionary<string, Table> v, Dictionary<string, Table> e) 
         {
@@ -67,158 +66,43 @@ namespace QueryEngine
             //Every query needs valid SELECT and MATCH expr.
             //Every query must end with semicolon ';'.
             Graph g = CreateGraph(args);
-            Query query = CreateQuery(reader, g.NodeTables, g.EdgeTables);
-            if (!query.CheckCorrectnessOfQuery()) throw new ArgumentException("Query is not correct, check assigned variables and their types.");
-            DFSPatternMatcher dfs = new DFSPatternMatcher(query.GetMatchPattern(),g);
-            dfs.Search();
-           
+            
+            while (true)
+            {
+                Console.WriteLine("Enter Query:");
+                Query query = CreateQuery(reader, g.NodeTables, g.EdgeTables);
+                if (!query.CheckCorrectnessOfQuery()) throw new ArgumentException("Query is not correct, check assigned variables and their types.");
+                Console.WriteLine();
+                Console.WriteLine("Results:");
+
+                DFSPatternMatcher dfs = new DFSPatternMatcher(query.GetMatchPattern(),g);
+                dfs.Search();
+
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.WriteLine("Continue? y/n (single character answer):");
+                string c;
+                c= (Console.ReadLine());
+                if (c[0] != 'y') break;
+                Console.Clear();
+            }
         }
+
+
 
         static void Main(string[] args)
         {
             try
             {
-              // Run(args, Console.In, Console.Out);
-             //  return;
+               Run(args, Console.In, Console.Out);
             }
             catch (Exception e )
             {
                 Console.WriteLine( e.Message);
             }
 
-            #region PRINT
-
-
-
-             Graph g = new Graph();
-            g.LoadNodeTables("VertexTypes.txt");
-           g.LoadEdgeTables("EdgeTypes.txt");
-           g.LoadEdgeList("NodesEdges.txt");
-
-
-            /* 
-
-
-           //just for testing
-           ///////////////////////////////////////
-
-
-           Scope scope = new Scope();
-           List<Token> tokens = Tokenizer.Tokenize(Console.In);
-
-           foreach (var item in tokens)
-           {
-             //  Console.WriteLine(item.type);
-            //  if (item.type == Token.TokenType.Identifier) Console.WriteLine(item.strValue) ;
-           }
-
-
-           SelectNode d = Parser.ParseSelectExpr(tokens);
-           MatchNode s = Parser.ParseMatchExpr(tokens);
-
-           SelectVisitor selectVisitor = new SelectVisitor();
-           MatchVisitor matchVisitor = new MatchVisitor(scope,g.NodeTables, g.EdgeTables);
-
-           d.Accept(selectVisitor);
-           var k = selectVisitor.GetResult();
-           s.Accept(matchVisitor);
-           var l = matchVisitor.GetResult();
-
-           Query q = new Query(new SelectObject(k), new MatchObject(l), scope);
-           Console.WriteLine(q.CheckCorrectnessOfQuery());
-           Console.ReadLine();
-
-
-
-           Console.WriteLine();
-           DFSPatternMatcher dfs = new DFSPatternMatcher(l, g);
-           dfs.Search();
-
-
-
-           Console.ReadLine();
-
-
-
-
-           /////////////
-           Console.WriteLine();
-           //Display whats inside dictionary of nodes 
-           foreach (var item in g.NodeTables)
-           {
-               Console.WriteLine("Key:"+item.Key);
-               Console.WriteLine("TableIri:"+item.Value.IRI);
-               foreach (var ite in item.Value.properties)
-               {
-                   Console.WriteLine("PropertyIRI:"+ite.IRI);
-                   Console.WriteLine("PropertyType:"+ ite.GetType());
-               }
-
-               Console.WriteLine();
-           }
-           Console.WriteLine();
-
-
-
-
-           /////////////
-
-           //Display whats inside dictionary of edges
-           foreach (var item in g.EdgeTables)
-           {
-               Console.WriteLine("Key:" + item.Key);
-               Console.WriteLine("TableIri:" + item.Value.IRI);
-               foreach (var ite in item.Value.properties)
-               {
-                   Console.WriteLine("PropertyIRI:" + ite.IRI);
-                   Console.WriteLine("PropertyType:" + ite.GetType());
-               }
-
-               Console.WriteLine();
-           }
-           Console.WriteLine() ;
-
-           ///
-           */
-
-           /////////////
-
-           Console.WriteLine("Vertices");
-           //Display whats inside vertices
-           foreach (var item in g.vertices)
-           {
-               Console.WriteLine("ID:"+item.id);
-               Console.WriteLine("TableIRI:" + item.table.IRI);
-               Console.WriteLine("OutSP:" + item.outEdgesStartPosition);
-                Console.WriteLine("OutEP:" + item.outEdgesEndPosition);
-
-                Console.WriteLine("InSP:" + item.inEdgesStartPosition);
-                Console.WriteLine("INEP:" + item.inEdgesEndPosition);
-               Console.WriteLine("P:" +item.GetPositionInVertices());
-               Console.WriteLine();
-           }
-           Console.WriteLine();
-           Console.WriteLine("OutEdges");
-           //displey whats inside edges
-           foreach (var item in g.outEdges)
-           {
-               Console.WriteLine("ID:" + item.id);
-               Console.WriteLine("TableIRI:" + item.table.IRI);
-               Console.WriteLine("EndVertexID:" + item.endVertex.id);
-               Console.WriteLine();
-           }
-           Console.WriteLine();
-           Console.WriteLine("InEdges");
-           foreach (var item in g.inEdges)
-           {
-               Console.WriteLine("ID:" + item.id);
-               Console.WriteLine("TableIRI:" + item.table.IRI);
-               Console.WriteLine("EndVertexID:" + item.endVertex.id);
-               Console.WriteLine();
-           }
-           Console.ReadLine();
-           /**/
-            #endregion PRINT
+            //TestClass.Run();
+            
         }
 
 
---
+++ QueryEngine/TestClass.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryEngine
+{
+    class TestClass
+    {
+        public static void Run()
+        {
+            #region PRINT
+
+
+ 
+             Graph g = new Graph();
+            g.LoadNodeTables("VertexTypes.txt");
+           g.LoadEdgeTables("EdgeTypes.txt");
+           g.LoadEdgeList("NodesEdges.txt");
+
+
+
+
+           //just for testing
+           ///////////////////////////////////////
+
+
+           Scope scope = new Scope();
+           List<Token> tokens = Tokenizer.Tokenize(Console.In);
+
+           foreach (var item in tokens)
+           {
+              Console.WriteLine(item.type);
+              if (item.type == Token.TokenType.Identifier) Console.WriteLine(item.strValue) ;
+           }
+
+
+           SelectNode d = Parser.ParseSelectExpr(tokens);
+           MatchNode s = Parser.ParseMatchExpr(tokens);
+
+           SelectVisitor selectVisitor = new SelectVisitor();
+           MatchVisitor matchVisitor = new MatchVisitor(scope,g.NodeTables, g.EdgeTables);
+
+           d.Accept(selectVisitor);
+           var k = selectVisitor.GetResult();
+           s.Accept(matchVisitor);
+           var l = matchVisitor.GetResult();
+
+           Query q = new Query(new SelectObject(k), new MatchObject(l), scope);
+           Console.WriteLine(q.CheckCorrectnessOfQuery());
+           Console.ReadLine();
+
+
+
+           Console.WriteLine();
+           DFSPatternMatcher dfs = new DFSPatternMatcher(l, g);
+           dfs.Search();
+
+
+
+           Console.ReadLine();
+
+
+
+
+           /////////////
+           Console.WriteLine();
+           //Display whats inside dictionary of nodes 
+           foreach (var item in g.NodeTables)
+           {
+               Console.WriteLine("Key:"+item.Key);
+               Console.WriteLine("TableIri:"+item.Value.IRI);
+               foreach (var ite in item.Value.properties)
+               {
+                   Console.WriteLine("PropertyIRI:"+ite.IRI);
+                   Console.WriteLine("PropertyType:"+ ite.GetType());
+               }
+
+               Console.WriteLine();
+           }
+           Console.WriteLine();
+
+
+
+
+           /////////////
+
+           //Display whats inside dictionary of edges
+           foreach (var item in g.EdgeTables)
+           {
+               Console.WriteLine("Key:" + item.Key);
+               Console.WriteLine("TableIri:" + item.Value.IRI);
+               foreach (var ite in item.Value.properties)
+               {
+                   Console.WriteLine("PropertyIRI:" + ite.IRI);
+                   Console.WriteLine("PropertyType:" + ite.GetType());
+               }
+
+               Console.WriteLine();
+           }
+           Console.WriteLine() ;
+
+           ///
+           
+
+           /////////////
+
+           Console.WriteLine("Vertices");
+           //Display whats inside vertices
+           foreach (var item in g.vertices)
+           {
+               Console.WriteLine("ID:"+item.id);
+               Console.WriteLine("TableIRI:" + item.table.IRI);
+               Console.WriteLine("OutSP:" + item.outEdgesStartPosition);
+                Console.WriteLine("OutEP:" + item.outEdgesEndPosition);
+
+                Console.WriteLine("InSP:" + item.inEdgesStartPosition);
+                Console.WriteLine("INEP:" + item.inEdgesEndPosition);
+               Console.WriteLine("P:" +item.GetPositionInVertices());
+               Console.WriteLine();
+           }
+           Console.WriteLine();
+           Console.WriteLine("OutEdges");
+           //displey whats inside edges
+           foreach (var item in g.outEdges)
+           {
+               Console.WriteLine("ID:" + item.id);
+               Console.WriteLine("TableIRI:" + item.table.IRI);
+               Console.WriteLine("EndVertexID:" + item.endVertex.id);
+               Console.WriteLine();
+           }
+           Console.WriteLine();
+           Console.WriteLine("InEdges");
+           foreach (var item in g.inEdges)
+           {
+               Console.WriteLine("ID:" + item.id);
+               Console.WriteLine("TableIRI:" + item.table.IRI);
+               Console.WriteLine("EndVertexID:" + item.endVertex.id);
+               Console.WriteLine();
+           }
+           Console.ReadLine();
+           
+            #endregion PRINT
+
+
+        }
+
+
+    }
+}
