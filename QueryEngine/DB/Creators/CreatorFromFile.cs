3d9838d1740f0a87c4ba72fe281e339fd12b10d0
goramartin <im.goramartin@gmail.com>
2020-12-12 22:20:19 +0100

    Visual changes

--- QueryEngine/DB/Creators/CreatorFromFile.cs
+++ QueryEngine/DB/Creators/CreatorFromFile.cs
@@ -10,7 +10,6 @@ creator can return desired object.
 
 */
 
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- QueryEngine/Extensions.cs
+++ QueryEngine/Extensions.cs
@@ -13,26 +13,26 @@ namespace QueryEngine
         {
             string tmpString = "Thread: " + Thread.CurrentThread.ManagedThreadId.ToString() + " result: ";
             for (int i = 0; i < tmp.Length; i++)
-            {
                 tmpString +=  " " + tmp[i].ID.ToString();
-            }
             return tmpString;
         }
 
+        /// <summary>
+        /// Populates array with the given value.
+        /// </summary>
         public static void Populate<T>(this T[] arr, T value)
         {
             for (int i = 0; i < arr.Length; i++)
-            {
                 arr[i] = value;
-            }
         }
 
+        /// <summary>
+        /// Populate array with increasing values of the given value.
+        /// </summary>
         public static void AscPopulate(this int[] arr, int value)
         {
             for (int i = value; i < arr.Length; i++)
-            {
                 arr[i] = i;
-            }
         }
 
     }
