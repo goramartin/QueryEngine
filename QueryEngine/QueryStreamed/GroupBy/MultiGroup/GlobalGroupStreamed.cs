e43f7568bd49659153a92954370fd92e6d409193
goramartin <im.goramartin@gmail.com>
2020-12-19 00:06:07 +0100

    Added matcher jobs to the streamed version

--- QueryEngine/QueryStreamed/GroupBy/MultiGroup/GlobalGroupStreamed.cs
+++ QueryEngine/QueryStreamed/GroupBy/MultiGroup/GlobalGroupStreamed.cs
@@ -1,28 +1,41 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace QueryEngine
 {
     internal class GlobalGroupStreamed : GroupResultProcessor
     {
+        ConcurrentDictionary<AggregateBucketResult[], AggregateBucketResult[]> globalGroups;
 
         public GlobalGroupStreamed(List<Aggregate> aggs, List<ExpressionHolder> hashes, IGroupByExecutionHelper helper, int columnCount) : base(aggs, hashes, helper, columnCount)
         {
+            
+            // to do
+
+
+
+
 
         }
 
         public override void Process(int matcherID, Element[] result)
         {
-            if (this.InParallel) ProcessParallel(matcherID, result);
+            if (result == null) return;
+            else if (this.InParallel) ProcessParallel(matcherID, result);
             else ProcessSingleThread(matcherID, result);
         }
 
 
         private void ProcessSingleThread(int matcherID, Element[] result)
         {
+
+
+
+
 
         }
 
@@ -36,7 +49,29 @@ namespace QueryEngine
 
         public override void RetrieveResults(out ITableResults resTable, out GroupByResults groupByResults)
         {
-            throw new NotImplementedException();
+            resTable = new TableResults();
+
+            // to do
+        }
+
+
+        private class MatcherJobST
+        {
+            public Dictionary<AggregateBucketResult[], AggregateBucketResult[]> groups;
+            // to do
+
+
+            public MatcherJobST(RowEqualityComparerAggregateBucketResult comparer)
+            {
+                this.groups = new Dictionary<AggregateBucketResult[], AggregateBucketResult[]>(comparer);
+            }
+        }
+
+        private class MatcherJobPar
+        {
+            // to do
+
+
         }
     }
 }
