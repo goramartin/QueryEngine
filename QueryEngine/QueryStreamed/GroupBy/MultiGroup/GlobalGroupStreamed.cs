0107be5343d4e82433534c1e5eb138c6b295fa91
goramartin <im.goramartin@gmail.com>
2020-12-18 23:56:06 +0100

    Layout of streamed global group class.

---
+++ QueryEngine/QueryStreamed/GroupBy/MultiGroup/GlobalGroupStreamed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryEngine
+{
+    internal class GlobalGroupStreamed : GroupResultProcessor
+    {
+
+        public GlobalGroupStreamed(List<Aggregate> aggs, List<ExpressionHolder> hashes, IGroupByExecutionHelper helper, int columnCount) : base(aggs, hashes, helper, columnCount)
+        {
+
+        }
+
+        public override void Process(int matcherID, Element[] result)
+        {
+            if (this.InParallel) ProcessParallel(matcherID, result);
+            else ProcessSingleThread(matcherID, result);
+        }
+
+
+        private void ProcessSingleThread(int matcherID, Element[] result)
+        {
+
+        }
+
+        private void ProcessParallel(int matcherID, Element[] result)
+        {
+
+
+
+
+        }
+
+        public override void RetrieveResults(out ITableResults resTable, out GroupByResults groupByResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
