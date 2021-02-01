ff302171e1d586d175d4556b91fad41c6bd3acee
goramartin <im.goramartin@gmail.com>
2021-02-01 18:21:45 +0100

    Added comments to the hs orderby class

--- QueryEngine/QueryStreamed/OrderBy/Sorter/ABTreeHalfStreamedSorter.cs
+++ QueryEngine/QueryStreamed/OrderBy/Sorter/ABTreeHalfStreamedSorter.cs
@@ -6,26 +6,36 @@ using System.Threading.Tasks;
 
 namespace QueryEngine
 {
+    /// <summary>
+    /// Class represents a half streamed order by.
+    /// Each matcher/thread orders it is found results locally using AB tree.
+    /// When the search is done, the results are merged using parallel Merge
+    /// from the HPCsharp library. This is done in two steps, firstly the 
+    /// ordered sequences from the ab trees are copied into an array which is
+    /// passed into the library functions. The merge function it self merges only two 
+    /// sub arrays of the array. Thus the merging tree has the height O(log(n)) where 
+    /// n is equal to the number of working threads/matchers.
+    /// </summary>
     internal class ABTreeHalfStreamedSorter : OrderByResultProcessor
     {
-        private Job[] matcherJobs;
+        private Job[] localSortJobs;
 
 
         public ABTreeHalfStreamedSorter(Graph graph, VariableMap variableMap, IOrderByExecutionHelper executionHelper, OrderByNode orderByNode, QueryExpressionInfo exprInfo, int columnCount) 
             : base(graph, variableMap, executionHelper, orderByNode, exprInfo, columnCount) 
         {
             var tmpComp = new RowComparer(this.comparers);
-            this.matcherJobs = new Job[this.executionHelper.ThreadCount];
-            for (int i = 0; i < matcherJobs.Length; i++)
+            this.localSortJobs = new Job[this.executionHelper.ThreadCount];
+            for (int i = 0; i < localSortJobs.Length; i++)
             {
                 var results = new TableResults(this.ColumnCount, this.executionHelper.FixedArraySize);
-                this.matcherJobs[i] = new Job(this.CreateComparer(tmpComp, results), results);
+                this.localSortJobs[i] = new Job(this.CreateComparer(tmpComp, results), results);
             }
         } 
 
         public override void Process(int matcherID, Element[] result)
         {
-            var tmpJob = this.matcherJobs[matcherID];
+            var tmpJob = this.localSortJobs[matcherID];
             if (result != null)
             {
                 tmpJob.results.StoreRow(result);
@@ -33,7 +43,7 @@ namespace QueryEngine
             } else
             {
                 // Parallel part
-                if (this.matcherJobs.Length > 1)
+                if (this.localSortJobs.Length > 1)
                 {
 
                 }
