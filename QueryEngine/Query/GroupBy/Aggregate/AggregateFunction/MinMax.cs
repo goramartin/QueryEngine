481c3d59c14c868dbe3b734ec685d88c65b7afde
goramartin <im.goramartin@gmail.com>
2021-02-15 23:12:40 +0100

    Adjusted min/max internal apply (parallel) so that after the first unsuccessful comparison, the compare exchange cycle ends immediately, since the value could be only exchanged for greater or smaller, thus the applied value can never be successful again.

--- QueryEngine/Query/GroupBy/Aggregate/AggregateFunction/MinMax.cs
+++ QueryEngine/Query/GroupBy/Aggregate/AggregateFunction/MinMax.cs
@@ -15,6 +15,8 @@ namespace QueryEngine
         protected abstract bool Compare(T x, T y);
         protected abstract bool CompareExchange(ref T value, T applied, T initial);
 
+
+        // Buckets
         public override void Apply(in TableResults.RowProxy row, AggregateBucketResult bucket)
         {
             if (this.expr.TryEvaluate(in row, out T returnValue))
@@ -23,7 +25,7 @@ namespace QueryEngine
                 if (tmpBucket.isSet)
                 {
                     if (Compare(tmpBucket.aggResult, returnValue)) tmpBucket.aggResult = returnValue;
-                    else { /* tmpBucket.aggResult > returnValue */ }
+                    else { }
                 }
                 else
                 {
@@ -48,7 +50,7 @@ namespace QueryEngine
                 if (tmpBucket.isSet)
                 {
                     if (Compare(tmpBucket.aggResult, returnValue)) tmpBucket.aggResult = returnValue;
-                    else { /* tmpBucket.aggResult > returnValue */ }
+                    else { }
                 }
                 else
                 {
@@ -72,7 +74,7 @@ namespace QueryEngine
             var tmpBucket1 = ((AggregateBucketResultWithSetFlag<T>)bucket1);
             var tmpBucket2 = ((AggregateBucketResultWithSetFlag<T>)bucket2);
             if (Compare(tmpBucket1.aggResult, tmpBucket2.aggResult)) tmpBucket1.aggResult = tmpBucket2.aggResult;
-            else { /* nothing */ }
+            else { }
         }
         public override void MergeThreadSafe(AggregateBucketResult bucket1, AggregateBucketResult bucket2)
         {
@@ -95,6 +97,8 @@ namespace QueryEngine
             MergeThreadSafeInternal(ref tmpBucket.aggResult, tmpList.aggResults[position]);
         }
       
+
+        // Lists
         public override void Apply(in TableResults.RowProxy row, AggregateListResults list, int position)
         {
             if (this.expr.TryEvaluate(in row, out T returnValue))
@@ -113,6 +117,7 @@ namespace QueryEngine
             else if (Compare(tmpList1.aggResults[into], tmpList2.aggResults[from])) tmpList1.aggResults[into] = tmpList2.aggResults[from];
         }
         
+        // Arrays
         public override void ApplyThreadSafe(in TableResults.RowProxy row, AggregateArrayResults array, int position)
         {
             if (this.expr.TryEvaluate(in row, out T returnValue))
@@ -130,7 +135,10 @@ namespace QueryEngine
             {
                 initialValue = value;
                 if (Compare(initialValue, applied)) replacement = applied;
-                else replacement = initialValue;
+                // The values can grow either up (max) or down (min).
+                // Because the comparison failed and in the meantime the value could be exchanged only for smaller (min) or greater (max).
+                // Thus, the applied value can never succeed in the first place.
+                else break; //replacement = initialValue;
             }
             while (!CompareExchange(ref value, replacement, initialValue));
         }
@@ -141,15 +149,7 @@ namespace QueryEngine
             {
                 if (isSet)
                 {
-                    // Compare-exchange mechanism.
-                    T initialValue, replacement;
-                    do
-                    {
-                        initialValue = value;
-                        if (Compare(initialValue, applied)) replacement = applied;
-                        else replacement = initialValue;
-                    }
-                    while (!CompareExchange(ref value, replacement, initialValue));
+                    MergeThreadSafeInternal(ref value, applied);
                     wasSet = true;
                 }
                 else
@@ -160,17 +160,20 @@ namespace QueryEngine
                         if (!isSet)
                         {
                             // The sets must be in this order, because after setting IsSet flag
-                            // there must be placed the value, otherwise thread could access empty bucket.
+                            // there must be placed value, otherwise thread could access empty bucket.
                             value = applied;
-                            isSet = true;
-                            wasSet = true;
+                            isSet = true; 
+                            wasSet = true;  
                         }
-                        else { /* next cycle it will go in the other branch of if(IsSet) */}
+                        else { /* Next cycle -> it will go in the other branch of if(IsSet). */}
                     }
                 }
             }
         }
     }
+
+    
+
 
 
     internal abstract class Min<T> : MinMaxBase<T>
