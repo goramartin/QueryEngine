bc36be63cf9b4b96a85e0b804e635abacede2fa1
goramartin <im.goramartin@gmail.com>
2021-02-17 18:24:32 +0100

    Adjusted streamed single group group by so that it can be computed in single thread manner

--- QueryEngine/QueryStreamed/GroupBy/SingleGroup/SingleGroupStreamed.cs
+++ QueryEngine/QueryStreamed/GroupBy/SingleGroup/SingleGroupStreamed.cs
@@ -21,7 +21,7 @@ namespace QueryEngine
         private Aggregate[] nonAsterixAggregates;
         private AggregateBucketResult[] nonAsterixResults;
         private bool containsAst = false;
-
+        
         public SingleGroupResultProcessorStreamed(QueryExpressionInfo expressionInfo, IGroupByExecutionHelper executionHelper, int columnCount) : base(expressionInfo, executionHelper, columnCount)
         {
             this.finalResults = AggregateBucketResult.CreateBucketResults(this.aggregates);
@@ -29,35 +29,58 @@ namespace QueryEngine
             if (this.finalResults.Length != this.nonAsterixResults.Length) this.containsAst = true;
         }
         
-
         /// <summary>
         /// If the given result is not null, the aggregates for the calling matcher are computed.
         /// The result == null means that the mather finished it's search.
         /// </summary>
         public override void Process(int matcherID, Element[] result)
         {
+            if (this.executionHelper.InParallel) this.ProcessParallel(result);
+            else this.ProcessSingleThread(result);
+        }
+
+        private void ProcessParallel(Element[] result)
+        {
             if (result != null)
             {
-                for (int i = 0; i < this.aggregates.Length; i++)
-                        this.aggregates[i].ApplyThreadSafe(result, finalResults[i]);
+                for (int i = 0; i < this.nonAsterixAggregates.Length; i++)
+                    this.nonAsterixAggregates[i].ApplyThreadSafe(result, this.nonAsterixResults[i]);
                 if (this.containsAst) Interlocked.Increment(ref this.numberOfMatchedElements);
                 else { }
-            } else
+            }
+            else
             {
                 // Signal that the matcher has finished.
                 var tmp = Interlocked.Increment(ref this.matchersFinished);
                 // The last finished matcher stores the number of matched elements.
                 if (tmp == this.executionHelper.ThreadCount)
                 {
-                    // Fill the Count(*) if presents.
                     if (this.containsAst)
-                    {
-                        for (int i = 0; i < this.aggregates.Length; i++)
-                            if (this.aggregates[i].IsAstCount) ((Count<int>)this.aggregates[i]).IncBy(this.numberOfMatchedElements, this.finalResults[i]);
-                    }
+                        this.FillAstCount();
                     // The final results will contain all the results.
                 }
             }
+        }
+
+        private void ProcessSingleThread(Element[] result)
+        {
+            if (result != null)
+            {
+                for (int i = 0; i < this.nonAsterixAggregates.Length; i++)
+                    this.nonAsterixAggregates[i].Apply(result, nonAsterixResults[i]);
+                if (this.containsAst) this.numberOfMatchedElements++;
+                else { }
+            } else
+            {
+                if (this.containsAst)
+                    this.FillAstCount();
+            }
+        }
+
+        private void FillAstCount()
+        {
+            for (int i = 0; i < this.aggregates.Length; i++)
+                if (this.aggregates[i].IsAstCount) ((Count<int>)this.aggregates[i]).IncBy(this.numberOfMatchedElements, this.finalResults[i]);
         }
 
         public override void RetrieveResults(out ITableResults resTable, out GroupByResults groupByResults)
