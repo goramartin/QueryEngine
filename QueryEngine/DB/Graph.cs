0f0d352c9cf06098896c4b6b60c25ebd9554e8fa
goramartin <im.goramartin@gmail.com>
2021-02-25 14:56:37 +0100

    Added checks for graph loading

--- QueryEngine/DB/Graph.cs
+++ QueryEngine/DB/Graph.cs
@@ -51,6 +51,7 @@ namespace QueryEngine
 
 
             Console.WriteLine("Loading tables...");
+
             this.LoadNodeTables("DataFiles\\NodeTypes.txt");
             this.LoadEdgeTables("DataFiles\\EdgeTypes.txt");
             Console.WriteLine("Loading tables finished.");
@@ -74,16 +75,24 @@ namespace QueryEngine
         /// <returns> Dictionary of tables. </returns>
         private Dictionary<string, Table> LoadTables(string filename)
         {
-            var reader = new TableFileReader(filename);
-            var processor = new TableDictProcessor();
-            processor.PassParameters(labels);
-            var creator = new CreatorFromFile<Dictionary<string, Table>>(reader, processor);
-            var tmpTables = creator.Create();
+            Dictionary<string, Table> tables = null;
+            try
+            {
+                var reader = new TableFileReader(filename);
+                var processor = new TableDictProcessor();
+                processor.PassParameters(labels);
+                var creator = new CreatorFromFile<Dictionary<string, Table>>(reader, processor);
+                tables = creator.Create();
+            } 
+            catch (Exception e)
+            {
+                throw new Exception($"Loading of the tables failed. File: {filename} / Error: {e.GetType()} {e.Message}. Try checking the format of the file.");
+            }
             
-            if (tmpTables.Count == 0) 
-                throw new ArgumentException($"{this.GetType()}, tables of the graph are empty. Filename = {filename}" );
+            if (tables == null || tables.Count == 0) 
+                throw new ArgumentException($"{this.GetType()}, tables of the graph are empty. File: {filename}" );
+            return tables;
 
-            return tmpTables;
         }
         private void LoadEdgeTables(string filename) => this.edgeTables = LoadTables(filename);
         private void LoadNodeTables(string filename) => this.nodeTables = LoadTables(filename);
@@ -94,13 +103,20 @@ namespace QueryEngine
         /// <param name="filename"> File containing data of vertices. </param>
         private void LoadVertices(string filename)
         {
-            var reader = new DataFileReader(filename);
-            var processor = new VerticesListProcessor();
-            processor.PassParameters(nodeTables);
-            var creator = new CreatorFromFile<List<Vertex>>(reader, processor);
-            this.vertices = creator.Create();
+            try
+            {
+                var reader = new DataFileReader(filename);
+                var processor = new VerticesListProcessor();
+                processor.PassParameters(nodeTables);
+                var creator = new CreatorFromFile<List<Vertex>>(reader, processor);
+                this.vertices = creator.Create();
+            } 
+            catch (Exception e)
+            {
+                throw new Exception($"Loading of the vertices failed. File: {filename} / Error: {e.GetType()} {e.Message}. Try checking the format of the file and the order of the properties based on the scheme.");
+            }
 
-            if (this.vertices.Count == 0) 
+            if (this.vertices == null || this.vertices.Count == 0) 
                 throw new ArgumentException($"{this.GetType()}, vertices of the graph are empty. Filename = {filename}");
         }
 
@@ -111,17 +127,24 @@ namespace QueryEngine
         /// <param name="filename"> File containing data of edges. </param>
         private void LoadEdges(string filename)
         {
-            var reader = new DataFileReader(filename);
-            var processor = new EdgeListProcessor();
-            processor.PassParameters(edgeTables, vertices);
-            var creator = new CreatorFromFile<EdgeListHolder>(reader, processor);
-            var result = creator.Create();
-            this.outEdges = result.outEdges;
-            this.inEdges = result.inEdges;
+            try
+            {
+                var reader = new DataFileReader(filename);
+                var processor = new EdgeListProcessor();
+                processor.PassParameters(edgeTables, vertices);
+                var creator = new CreatorFromFile<EdgeListHolder>(reader, processor);
+                var result = creator.Create();
+                this.outEdges = result.outEdges;
+                this.inEdges = result.inEdges;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Loading of the edges failed. File: {filename} / Error: {e.GetType()} {e.Message}. Try checking the format of the file and the order of the properties based on the scheme.");
+            }
 
-            if (this.outEdges.Count == 0) 
+            if (this.outEdges == null || this.outEdges.Count == 0) 
                 throw new ArgumentException($"{this.GetType()} Out edges of the graph are empty. Filename = {filename}");
-            if (this.inEdges.Count == 0) 
+            if (this.inEdges == null || this.inEdges.Count == 0) 
                 throw new ArgumentException($"{this.GetType()} In edges of the graph are empty. Filename = {filename}");
 
         }
